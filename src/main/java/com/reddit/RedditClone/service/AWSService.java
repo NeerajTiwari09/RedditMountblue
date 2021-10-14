056e8a2a9d46206e411c38b1d3d77c6370f5d7ae
Adarsh Gupta <87adarsh.gupta@gmail.com>
2021-10-15 00:53:44 +0530

    uploading feature from client to AWS S3 bucket.

---
+++ src/main/java/com/reddit/RedditClone/service/AWSService.java
@@ -0,0 +1,41 @@
+package com.reddit.RedditClone.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class AWSService {
+
+    private String bucketName = "reddit-clone-spring-boot";
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete();
+        return s3Client.getUrl(bucketName, fileName).toString();
+    }
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
+
+}
