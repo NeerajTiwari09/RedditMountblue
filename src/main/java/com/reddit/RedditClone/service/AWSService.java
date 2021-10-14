294f3f9267f9b7bf9058be2c459e6fad54e69401
Mani-MB <mani.chintalappagari.17.1@mountblue.tech>
2021-10-15 01:48:49 +0530

    added code for download and delete files to AWSServiece

--- src/main/java/com/reddit/RedditClone/service/AWSService.java
+++ src/main/java/com/reddit/RedditClone/service/AWSService.java
@@ -2,6 +2,9 @@ package com.reddit.RedditClone.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +39,23 @@ public class AWSService {
             log.error("Error converting multipartFile to file", e);
         }
         return convertedFile;
+    }
+
+    public byte[] downloadFile(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String deleteFile(String fileName) {
+        s3Client.deleteObject(bucketName, fileName);
+        return fileName + " removed ...";
     }
 
 }
