8564ee49dab274229cf21aa53818fe89953a18c4
swabeeh om <swabeeh.om.17.1@mountblue.tech>
2021-10-15 17:29:09 -0700

    model class for vote created

---
+++ src/main/java/com/reddit/RedditClone/model/Vote.java
@@ -0,0 +1,23 @@
+package com.reddit.RedditClone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "votes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean vote;
+    private Long postId;
+    private Long userId;
+}
