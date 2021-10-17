912a3754381b9b82a2a60a42b9285ff8f8855442
Mani-MB <mani.chintalappagari.17.1@mountblue.tech>
2021-10-17 13:06:15 +0530

    created model class for the threaded comments

---
+++ src/main/java/com/reddit/RedditClone/model/Comment.java
@@ -0,0 +1,93 @@
+package com.reddit.RedditClone.model;
+
+import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.reddit.RedditClone.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+@Getter
+@Setter
+public class Comment implements Comparable<Comment> {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(length=5000)
+    private String text;
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+    @ManyToOne
+    @JsonIgnore
+    private Post post;
+    @OneToMany(mappedBy="comment")
+    @OrderBy("createdDate, id")
+    private SortedSet<Comment> comments = new TreeSet<>();
+    @ManyToOne
+    @JoinColumn(name="comment_id", nullable=true)
+    @JsonIgnore
+    private Comment comment;
+    private Date createdDate;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Comment other = (Comment) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Comment that) {
+        int comparedValue = this.createdDate.compareTo(that.createdDate);
+        if (comparedValue == 0) {
+            comparedValue = this.id.compareTo(that.id);
+        }
+        return comparedValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
